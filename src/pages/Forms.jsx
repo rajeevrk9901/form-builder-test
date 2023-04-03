06b9abb2c246785a604b8486d80c138bcbf91333
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-04-03 22:32:28 +0530

    added colums layout jsons

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -178,6 +178,240 @@ const Forms = () => {
         ],
     };
 
+
+    // First Name Last Name
+    const fnln = {
+        "components": [
+            {
+                "input": false,
+                "key": "columns",
+                "label": "Columns",
+                "type": "columns",
+                "columns": [
+                    {
+                        "components": [
+                            {
+                                "label": "First Name",
+                                "allowMultipleMasks": false,
+                                "inputMasks": [
+                                    {
+                                        "label": "",
+                                        "mask": ""
+                                    }
+                                ],
+                                "type": "textfield",
+                                "input": true,
+                                "key": "firstName"
+                            }
+                        ],
+                        "width": 6,
+                        "offset": 0,
+                        "push": 0,
+                        "pull": 0,
+                        "type": "column",
+                        "input": true,
+                        "key": "",
+                        "label": ""
+                    },
+                    {
+                        "components": [
+                            {
+                                "label": "Last Name",
+                                "allowMultipleMasks": false,
+                                "inputMasks": [
+                                    {
+                                        "label": "",
+                                        "mask": ""
+                                    }
+                                ],
+                                "type": "textfield",
+                                "input": true,
+                                "key": "lastName"
+                            }
+                        ],
+                        "width": 6,
+                        "offset": 0,
+                        "push": 0,
+                        "pull": 0,
+                        "type": "column",
+                        "input": true,
+                        "key": "",
+                        "label": ""
+                    }
+                ]
+            },
+            {
+                "type": "button",
+                "label": "Submit",
+                "key": "submit",
+                "disableOnInvalid": true,
+                "theme": "primary",
+                "input": true
+            }
+        ],
+        "display": "form"
+    }
+
+    const contactNum = {
+        "components": [
+            {
+                "columns": [
+                    {
+                        "components": [
+                            {
+                                "label": "Country Code",
+                                "mask": false,
+                                "type": "select",
+                                "input": true,
+                                "key": "select",
+                                "defaultValue": "",
+                                "data": {
+                                    "values": {
+                                        "0": {
+                                            "label": "+91",
+                                            "value": "91"
+                                        }
+                                    }
+                                },
+                                "valueProperty": "value",
+                                "limit": null,
+                                "refreshOn": null,
+                                "url": "",
+                                "resource": null,
+                                "selectValues": "",
+                                "disableLimit": false,
+                                "searchField": "",
+                                "clearOnRefresh": false,
+                                "reference": false
+                            }
+                        ],
+                        "width": 2,
+                        "offset": 0,
+                        "push": 0,
+                        "pull": 0,
+                        "type": "column",
+                        "input": true,
+                        "key": "",
+                        "label": ""
+                    },
+                    {
+                        "components": [
+                            {
+                                "label": "Mobile Number",
+                                "allowMultipleMasks": false,
+                                "inputMasks": [
+                                    {
+                                        "label": "",
+                                        "mask": ""
+                                    }
+                                ],
+                                "type": "textfield",
+                                "input": true,
+                                "key": "textField2"
+                            }
+                        ],
+                        "width": 10,
+                        "offset": 0,
+                        "push": 0,
+                        "pull": 0,
+                        "type": "column",
+                        "input": true,
+                        "key": "",
+                        "label": ""
+                    }
+                ],
+                "label": "Columns",
+                "mask": false,
+                "type": "columns",
+                "input": false,
+                "key": "columns"
+            },
+            {
+                "type": "button",
+                "label": "Submit",
+                "key": "submit",
+                "disableOnInvalid": true,
+                "theme": "primary",
+                "input": true
+            }
+        ],
+        "display": "form"
+    }
+
+    const countDist = {
+        "display": "form",
+        "components": [
+            {
+                "label": "Country",
+                "widget": "choicesjs",
+                "dataSrc": "url",
+                "data": {
+                    "url": "https://restcountries.com/v3.1/all?fields=name.common,alpha2Code"
+                },
+                "valueProperty": "alpha2Code",
+                "template": "<span>{{ item.name.common }}</span>",
+                "refreshOn": "",
+                "multiple": false,
+                "validate": {
+                    "required": true
+                },
+                "key": "country",
+                "type": "select",
+                "input": true
+            },
+            {
+                "label": "State/Province",
+                "widget": "choicesjs",
+                "dataSrc": "url",
+                "data": {
+                    "url": "https://restcountries.com/v3.1/alpha/{{ data.country }}/subdivisions?fields=name,code"
+                },
+                "valueProperty": "code",
+                "template": "<span>{{ item.name }}</span>",
+                "refreshOn": "data.country",
+                "multiple": false,
+                "conditional": {
+                    "show": true,
+                    "when": "country",
+                    "eq": "{{ item.code }}"
+                },
+                "validate": {
+                    "required": true
+                },
+                "key": "state",
+                "type": "select",
+                "input": true
+            },
+            {
+                "label": "District",
+                "widget": "choicesjs",
+                "dataSrc": "url",
+                "data": {
+                    "url": "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/{{ data.country }}/states/{{ data.state }}-districts.json",
+                    "valueProperty": "properties.NAME_2"
+                },
+                "valueProperty": "properties.NAME_2",
+                "template": "<span>{{ item.properties.NAME_2 }}</span>",
+                "refreshOn": "data.state",
+                "multiple": false,
+                "conditional": {
+                    "show": true,
+                    "when": "state",
+                    "eq": "{{ item.code }}"
+                },
+                "validate": {
+                    "required": true
+                },
+                "key": "district",
+                "type": "select",
+                "input": true
+            }
+        ]
+    }
+
+
+
+
     const onSubmit = (submission) => {
         console.log('Form data:', submission.data);
     };
@@ -192,10 +426,10 @@ const Forms = () => {
         // <div className='w-56 flex justify-center'>
         //   <Form src={formSchema} />
         // </div>
-
+        // contactNum fnln
         <div className='w-full max-w-lg mx-auto p-6'>
             <h1 className='text-3xl font-bold mb-6'>Contact Us</h1>
-            <Form src={form1} onSubmit={onSubmit} />
+            <Form src={countDist} onSubmit={onSubmit} />
         </div>
 
     );
