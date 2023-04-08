be13b7999c5369510e93e6b35aee91c8c7c141c6
Mayank <you@example.com>
2023-04-08 15:32:29 +0530

    added json of forms

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-
+import country from "../Data/Country.json"
+import permaAdd from "../Data/PermanentAddress.json"
 import { Form, FormBuilder } from '@formio/react';
 // import "formiojs/dist/formio.full.css";
 // import Form from '@formio/react/lib/components/Form';
@@ -127,6 +128,394 @@ const Forms = () => {
         ],
         "display": "form"
     }
+
+   const form2= {
+    "components": [
+        {
+            "label": "Name",
+            "allowMultipleMasks": false,
+            "inputMasks": [
+                {
+                    "label": "",
+                    "mask": ""
+                }
+            ],
+            "type": "textfield",
+            "input": true,
+            "key": "name",
+            "validate": {
+                "required": true,
+                "minLength": null,
+                "maxLength": null,
+                "unique": false,
+                "customMessage": "",
+                "json": ""
+            }
+        },
+        {
+            "label": "Email",
+            "type": "email",
+            "input": true,
+            "key": "email2",
+            "validate": {
+                "unique": true,
+                "customMessage": "",
+                "json": "",
+                "required": true
+            }
+        },
+        {
+            "label": "Phone Number",
+            "inputMask": "(+99) 9999999999",
+            "allowMultipleMasks": false,
+            "inputMasks": [
+                {
+                    "label": "",
+                    "mask": ""
+                }
+            ],
+            "type": "phoneNumber",
+            "input": true,
+            "key": "phoneNumber2",
+            "defaultValue": "(+91) __________",
+            "validate": {
+                "minLength": null,
+                "maxLength": null,
+                "unique": true,
+                "customMessage": "",
+                "json": "",
+                "required": true
+            },
+            "conditional": {
+                "json": ""
+            },
+            "properties": [
+                {
+                    "key": "",
+                    "value": ""
+                }
+            ],
+            "logic": [],
+            "customConditional": "",
+            "tags": []
+        },
+        {
+            "label": "Aadhar",
+            "allowMultipleMasks": false,
+            "inputMasks": [
+                {
+                    "label": "",
+                    "mask": ""
+                }
+            ],
+            "type": "textfield",
+            "input": true,
+            "key": "aadhar",
+            "validate": {
+                "required": true,
+                "minLength": null,
+                "maxLength": null,
+                "pattern": "^[0-9]{4}[ -]?[0-9]{4}[ -]?[0-9]{4}$",
+                "unique": true,
+                "customMessage": "Please Enter Valid Aadhar Number",
+                "json": ""
+            }
+        },
+        {
+            "label": "Pan",
+            "allowMultipleMasks": false,
+            "inputMasks": [
+                {
+                    "label": "",
+                    "mask": ""
+                }
+            ],
+            "type": "textfield",
+            "input": true,
+            "key": "pan",
+            "defaultValue": "",
+            "validate": {
+                "required": true,
+                "minLength": null,
+                "maxLength": null,
+                "pattern": "[A-Z]{5}[0-9]{4}[A-Z]{1}",
+                "unique": true,
+                "customMessage": "",
+                "json": ""
+            }
+        },
+        {
+            "label": "Date Of Birth",
+            "mask": false,
+            "type": "day",
+            "input": true,
+            "key": "dob",
+            "fields": {
+                "day": {
+                    "type": "number"
+                },
+                "month": {
+                    "type": "select"
+                },
+                "year": {
+                    "type": "number"
+                }
+            },
+            "defaultValue": "1/31/1995",
+            "validate": {
+                "unique": false,
+                "customMessage": "",
+                "json": ""
+            }
+        },
+        {
+            "optionsLabelPosition": "right",
+            "values": {
+                "0": {
+                    "label": "MALE",
+                    "value": "male"
+                },
+                "1": {
+                    "label": "FEMALE",
+                    "value": "female"
+                }
+            },
+            "label": "Gender",
+            "inline": true,
+            "mask": false,
+            "type": "radio",
+            "input": true,
+            "key": "gender"
+        },
+        {
+            "map": {
+                "key": ""
+            },
+            "label": "Current Address",
+            "type": "address",
+            "input": true,
+            "key": "currentAddress",
+            "validate": {
+                "required": true,
+                "unique": false,
+                "customMessage": "",
+                "json": ""
+            },
+            "properties": [
+                {
+                    "key": "",
+                    "value": ""
+                }
+            ],
+            "logic": []
+        },
+        {
+            "columns": [
+                {
+                    "components": [
+                        {
+                            "label": "Country",
+                            "mask": false,
+                            "type": "select",
+                            "input": true,
+                            "key": "select",
+                            "defaultValue": "",
+                            "validate": {
+                                "required": true,
+                                "select": false,
+                                "unique": false,
+                                "customMessage": "",
+                                "json": ""
+                            },
+                            "conditional": {
+                                "json": ""
+                            },
+                            "data": {
+                                "json":country
+                            },
+                            "properties": [
+                                {
+                                    "key": "",
+                                    "value": ""
+                                }
+                            ],
+                            "tags": [],
+                            "customConditional": "",
+                            "valueProperty": "value",
+                            "url": "",
+                            "resource": null,
+                            "selectValues": "",
+                            "disableLimit": false,
+                            "searchField": "",
+                            "limit": null,
+                            "refreshOn": null,
+                            "clearOnRefresh": false,
+                            "reference": false
+                        },
+                        {
+                            "label": "District",
+                            "mask": false,
+                            "type": "select",
+                            "input": true,
+                            "key": "district",
+                            "valueProperty": "value"
+                        }
+                    ],
+                    "width": 6,
+                    "offset": 0,
+                    "push": 0,
+                    "pull": 0,
+                    "type": "column",
+                    "input": true,
+                    "key": "",
+                    "label": ""
+                },
+                {
+                    "components": [
+                        {
+                            "label": "State",
+                            "mask": false,
+                            "type": "select",
+                            "input": true,
+                            "key": "state",
+                            "valueProperty": "value"
+                        },
+                        {
+                            "label": "City",
+                            "mask": false,
+                            "type": "select",
+                            "input": true,
+                            "key": "city",
+                            "valueProperty": "value"
+                        }
+                    ],
+                    "width": 6,
+                    "offset": 0,
+                    "push": 0,
+                    "pull": 0,
+                    "type": "column",
+                    "input": true,
+                    "key": "",
+                    "label": ""
+                }
+            ],
+            "label": "CurrenAddress",
+            "mask": false,
+            "type": "columns",
+            "input": false,
+            "key": "currenAddress"
+        },
+        {
+            "label": "Same as Current Address",
+            "mask": false,
+            "type": "checkbox",
+            "input": true,
+            "key": "sameAsCurrentAddress"
+        },
+        {
+            "map": {
+                "key": ""
+            },
+            "label": "Permanent Address",
+            "type": "address",
+            "input": true,
+            "key": "permanentAddress",
+            "validate": {
+                "unique": false,
+                "customMessage": "",
+                "json": ""
+            },
+            "properties": [
+                {
+                    "key": "County",
+                    "value": "123"
+                },
+                {
+                    "key": "",
+                    "value": ""
+                }
+            ],
+            "tags": []
+        },
+        {
+            "columns": [
+                {
+                    "components": [
+                        {
+                            "label": "Country",
+                            "mask": false,
+                            "type": "select",
+                            "input": true,
+                            "key": "country",
+                            "valueProperty": "value"
+                        },
+                        {
+                            "label": "District",
+                            "mask": false,
+                            "type": "select",
+                            "input": true,
+                            "key": "district2",
+                            "valueProperty": "value"
+                        }
+                    ],
+                    "width": 6,
+                    "offset": 0,
+                    "push": 0,
+                    "pull": 0,
+                    "type": "column",
+                    "input": true,
+                    "key": "",
+                    "label": ""
+                },
+                {
+                    "components": [
+                        {
+                            "label": "State",
+                            "mask": false,
+                            "type": "select",
+                            "input": true,
+                            "key": "state2",
+                            "valueProperty": "value"
+                        },
+                        {
+                            "label": "City",
+                            "mask": false,
+                            "type": "select",
+                            "input": true,
+                            "key": "city2",
+                            "valueProperty": "value"
+                        }
+                    ],
+                    "width": 6,
+                    "offset": 0,
+                    "push": 0,
+                    "pull": 0,
+                    "type": "column",
+                    "input": true,
+                    "key": "",
+                    "label": ""
+                }
+            ],
+            "label": "Columns",
+            "mask": false,
+            "type": "columns",
+            "input": false,
+            "key": "columns"
+        },
+        {
+            "type": "button",
+            "label": "Submit",
+            "key": "submit",
+            "disableOnInvalid": true,
+            "theme": "primary",
+            "input": true
+        }
+
+        
+    ],
+    "display": "form"
+}
+
 
     // const form = {
     //   name: 'My Form',
@@ -338,77 +727,6 @@ const Forms = () => {
         "display": "form"
     }
 
-    const countDist = {
-        "display": "form",
-        "components": [
-            {
-                "label": "Country",
-                "widget": "choicesjs",
-                "dataSrc": "url",
-                "data": {
-                    "url": "https://restcountries.com/v3.1/all?fields=name.common,alpha2Code"
-                },
-                "valueProperty": "alpha2Code",
-                "template": "<span>{{ item.name.common }}</span>",
-                "refreshOn": "",
-                "multiple": false,
-                "validate": {
-                    "required": true
-                },
-                "key": "country",
-                "type": "select",
-                "input": true
-            },
-            {
-                "label": "State/Province",
-                "widget": "choicesjs",
-                "dataSrc": "url",
-                "data": {
-                    "url": "https://restcountries.com/v3.1/alpha/{{ data.country }}/subdivisions?fields=name,code"
-                },
-                "valueProperty": "code",
-                "template": "<span>{{ item.name }}</span>",
-                "refreshOn": "data.country",
-                "multiple": false,
-                "conditional": {
-                    "show": true,
-                    "when": "country",
-                    "eq": "{{ item.code }}"
-                },
-                "validate": {
-                    "required": true
-                },
-                "key": "state",
-                "type": "select",
-                "input": true
-            },
-            {
-                "label": "District",
-                "widget": "choicesjs",
-                "dataSrc": "url",
-                "data": {
-                    "url": "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/{{ data.country }}/states/{{ data.state }}-districts.json",
-                    "valueProperty": "properties.NAME_2"
-                },
-                "valueProperty": "properties.NAME_2",
-                "template": "<span>{{ item.properties.NAME_2 }}</span>",
-                "refreshOn": "data.state",
-                "multiple": false,
-                "conditional": {
-                    "show": true,
-                    "when": "state",
-                    "eq": "{{ item.code }}"
-                },
-                "validate": {
-                    "required": true
-                },
-                "key": "district",
-                "type": "select",
-                "input": true
-            }
-        ]
-    }
-
 
 
 
@@ -429,10 +747,10 @@ const Forms = () => {
         // contactNum fnln
         <div className='w-full max-w-lg mx-auto p-6'>
             <h1 className='text-3xl font-bold mb-6'>Contact Us</h1>
-            <Form src={countDist} onSubmit={onSubmit} />
+            <Form src={permaAdd} onSubmit={onSubmit} />
         </div>
 
     );
-};
+}; 
 
 export default Forms;
